c668b77f8f1b3266ac015e06fdb7d8e651ee955f
koome <leonkoome4@gmail.com>
2026-04-15 12:09:19 +0300

    one

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+require("datejs");
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
+  for (i = 0; i < args.length; i++) {
+    const userArray = args[i];
+    combinedObject.users.push(...userArray);
+  }
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
 
+  return combinedObject;
+}
 
 
 module.exports = {
